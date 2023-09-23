14552e8a36131625ea93906ef4d23eb1549e95e6
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-23 13:19:49 +0300

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,6 +28,7 @@ def c(text):
     return 'C {}'.format(text.replace("_", " "))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """display custom text given"""
